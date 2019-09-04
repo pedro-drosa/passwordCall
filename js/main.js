5d3eb2406b2529fcfa9684660d031fdf4d45499a
Pedro Juraci <pedro_drosa@hotmail.com>
2019-09-04 10:12:31 -0300

    refactoring the project

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
-let section = document.querySelector('[data-value]');
-window.addEventListener('click',nextPass);
-function nextPass(){
-    section.dataset.value < 99 ? section.dataset.value++: section.dataset.value = 0; 
-    section.dataset.value < 10 ? section.children[0].innerText = "0"+section.dataset.value: section.children[0].innerText = section.dataset.value;
+class Call{
+    constructor()
+    {
+        this._numberEl = document.querySelector('[data-value="number"]')
+        window.addEventListener('click',()=>{
+            this.nextPass()
+        })
+    }
+    get numberEl()
+    {
+        return Number.parseInt(this._numberEl.innerHTML)
+    }
+    set numberEl(value)
+    {
+        this._numberEl.innerHTML = value
+    }
+    nextPass()
+    {
+        this.numberEl < 99 ? this.numberEl++ : this.numberEl = 0
+        this.numberEl < 10 ? this.numberEl = `0${this.numberEl++}` : this.numberEl++
+    }
 }
+const call = new Call()
