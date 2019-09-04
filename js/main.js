2de37d52aeac73ff61e77e4d93588b75fa37bb83
Pedro Juraci <pedro_drosa@hotmail.com>
2019-09-04 10:34:43 -0300

    new features

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 class Call{
     constructor()
     {
+        this._audio = new Audio('beep.mp3')
+        this._audioIsRunning = false
         this._numberEl = document.querySelector('[data-value="number"]')
         window.addEventListener('click',()=>{
             this.nextPass()
@@ -18,6 +20,15 @@ class Call{
     {
         this.numberEl < 99 ? this.numberEl++ : this.numberEl = 0
         this.numberEl < 10 ? this.numberEl = `0${this.numberEl++}` : this.numberEl++
+        
+        this.playAudio()
+    }
+    playAudio()
+    {
+        if(this._audio){
+            this._audio.currentTime = 0 
+            this._audio.play()
+        }
     }
 }
 const call = new Call()
